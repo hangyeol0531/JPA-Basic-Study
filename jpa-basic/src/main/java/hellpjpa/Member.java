a5e22bdb66aeb1be16640e78fcec108be0dbab9a
hangyeol0531 <jhan0531@naver.com>
2022-09-24 15:33:53 +0900

    one-way Associated relationship

--- jpa-basic/src/main/java/hellpjpa/Member.java
+++ jpa-basic/src/main/java/hellpjpa/Member.java
@@ -9,20 +9,20 @@ import java.util.Date;
 @Getter
 @Setter
 @Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ",
-        initialValue = 1, allocationSize = 50)
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
 
+    @Id @GeneratedValue()
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "USERNAME")
     private String username;
 
-    public Member(){
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
-    }
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
 }
