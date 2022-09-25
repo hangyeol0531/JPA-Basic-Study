79c4f2b34f062bfb5eaee64f5f880a65975932d6
hangyeol0531 <jhan0531@naver.com>
2022-09-25 17:29:32 +0900

    CASCADE

---
+++ jpa-basic/src/main/java/hellpjpa/Child.java
@@ -0,0 +1,22 @@
+package hellpjpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Child {
+    @Id
+    @GeneratedValue()
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Parent parent;
+}
